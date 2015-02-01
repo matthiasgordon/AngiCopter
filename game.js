876441763d8e5bc7857bf0e3237d46954181a3e3
Matthias <gordon.matthias@googlemail.com>
2015-02-01 22:33:53 +0100

    Verbesserung des ersten Versuches. Bisher ist es möglich einen Block mit den Pfeiltasten zu bewegen.

--- game.js
+++ game.js
@@ -1,54 +1,71 @@
+//ctx.fillStyle = "white";
+//fillRect(X, Y, width, height);
+//ctx.fillRect(0, 0, w, h);
+//ctx.strokeStyle = "black";
+//ctx.strokeRect(0, 0, w, h);
+
 $(document).ready(function(){
-	//Canvas stuff
 	var canvas = $("#canvas")[0];
 	var ctx = canvas.getContext("2d");
 	var w = $("#canvas").width();
 	var h = $("#canvas").height();
 
-	//ctx.fillStyle = "white";
-	//fillRect(X, Y, width, height);
-	//ctx.fillRect(0, 0, w, h);
-	//ctx.strokeStyle = "black";
-	//ctx.strokeRect(0, 0, w, h);
-
-	ctx.fillStyle = "black";
-	ctx.fillRect(390,580,20,20);
+	var interval;
+	var FPS = 30;
 
 	var x=0;
 	var y=0;
 
-	document.addEventListener("keydown",keyDownHandler, false);
-
-	function keyDownHandler(event) {
-		var keyPressed = String.fromCharCode(event.keyCode);
-
-		if(keyPressed == "W") {
-			x = x + 5;
-			ctx.clearRect ( 0 , 0 , canvas.width, canvas.height );
-			ctx.fillStyle = "black";
-			ctx.fillRect(390-y,580-x,20,20);
+	var taxi = {
+		color: "#aa0000",
+		x: w/2-10,
+		y: h-20,
+		width: 20,
+		height: 20,
+		draw: function() {
+			ctx.fillStyle = this.color;
+			ctx.fillRect(this.x,this.y,this.width,this.height);
 		}
+	};
 
-		if(keyPressed == "S") {
-			x = x - 5;
-			ctx.clearRect ( 0 , 0 , canvas.width, canvas.height );
-			ctx.fillStyle = "black";
-			ctx.fillRect(390-y,580-x,20,20);
-		}
 
-		if(keyPressed == "A") {
-			y = y + 5;
-			ctx.clearRect ( 0 , 0 , canvas.width, canvas.height );
-			ctx.fillStyle = "black";
-			ctx.fillRect(390-y,580-x,20,20);
-		}
 
-		if(keyPressed == "D") {
-			y = y - 5;
-			ctx.clearRect ( 0 , 0 , canvas.width, canvas.height );
-			ctx.fillStyle = "black";
-			ctx.fillRect(390-y,580-x,20,20);
-		}
+	$(document).bind("keydown", function() {});
+
+	
+
+	function draw() {
+		ctx.clearRect ( 0 , 0 , canvas.width, canvas.height );
+		taxi.draw();
 	}
+
+	function update() {
+		if(keydown.up) {
+			taxi.y -= 5;
+		}
+		if(keydown.down) {
+			taxi.y += 5;
+		}
+		if (keydown.left) {
+	    	taxi.x -= 5;
+	  	}
+	  	if (keydown.right) {
+	    	taxi.x += 5;
+	  	}
+	  	if(keydown.space) {
+	  		//Zeit die Kufen auszufahren!!!
+	  	}
+	}
+
+	
+	setInterval(function() {
+ 		update();
+  		draw();
+	}, 1000/FPS);
+
+
+	
+
+
 
 })
