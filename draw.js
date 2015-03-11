968e907d8ed446c0d9cdd351c59bdf77a1806c77
Matthias <gordon.matthias@googlemail.com>
2015-03-11 20:36:03 +0100

    Anpassung Vorschriften für Levelbeschreibung
    - drawLevel wurde so angepasst, dass es den Vorschriften zur
    Levelbeschreibung entspricht
    - die alten Level wurden in den Ordner alte Level verschoben
    - ein neues Level wurde eingefügt, dass den Vorschriften zur
    Levelbeschreibungen entspricht. Dieses Level wird per default geladen

--- draw.js
+++ draw.js
@@ -13,62 +13,71 @@ function drawLevel() {
         for (x = 0; x < levelXMax; x++) {
 
             switch(levelRows[y][x]){
+
                 //Basic Level elements
                 case ".": //Nothing
-                    //ctx.fillStyle = "#AAAA00";
-                    //ctx.fillRect(x * blockSizeX, y * blockSizeY, blockSizeX, blockSizeY);
+
                     break;
-                case "#": //Platform
+
+                // Platform
+                case "#":
                     ctx.fillStyle = "#AAAAAA";
                     ctx.fillRect(x * blockSizeX, y * blockSizeY, blockSizeX, blockSizeY);
                     break;
-                case "R": //frame
-                    ctx.drawImage(edge, 0, 0, edge.width, edge.height, x * blockSizeX, y * blockSizeY, blockSizeX, blockSizeY);
-                    break;
 
-                //Extended Level Elements                             
-                case "A": 
-                    ctx.drawImage(obstacle, 0, 0, obstacle.width, obstacle.height, x * blockSizeX, y * blockSizeY, blockSizeX, blockSizeY);
-                    break;
-
-                case "F":
-                    ctx.drawImage(fire, Math.floor(frame % 7) * fire.width / 7, 0, fire.width / 7, fire.height,
-			                             x * blockSizeX, y * blockSizeY, blockSizeX, blockSizeY);
-                    break;
-                
-                //Jungle platform <===>
+                // Left edge of platform
                 case "<":
                     ctx.drawImage(blocks20x10, blocks20x10.width / 20 * 5, blocks20x10.height / 10 * 1, blocks20x10.width / 20, blocks20x10.height / 10,
                                         x * blockSizeX, y * blockSizeY, blockSizeX, blockSizeY);
                     break;
                 
-                case "=":
-                    var indexx = 6, indexy = 1;
-                    ctx.drawImage(blocks20x10, blocks20x10.width / 20 * indexx, blocks20x10.height / 10 * indexy, blocks20x10.width / 20, blocks20x10.height / 10,
-                                        x * blockSizeX, y * blockSizeY, blockSizeX, blockSizeY);
-                    break;
-
+                // Right edge of platform
                 case ">":
                     var indexx = 10, indexy = 1;
                     ctx.drawImage(blocks20x10, blocks20x10.width / 20 * indexx, blocks20x10.height / 10 * indexy, blocks20x10.width / 20, blocks20x10.height / 10,
                                         x * blockSizeX, y * blockSizeY, blockSizeX, blockSizeY);
                     break;
 
-                // Dynamic level elements
-                // Taxi and guests
-                // Diese Elemente mussen an sich dynamisch gezeichnet werden - hier nur für Demozwecke zeichnen
-                /* case "1":
-                    ctx.drawImage(taxi, 0, 0, taxi.width, taxi.height, x * blockSizeX, y * blockSizeY, blockSizeX, blockSizeY);
+                // Level frame
+                case "R":
+                    ctx.drawImage(edge, 0, 0, edge.width, edge.height, x * blockSizeX, y * blockSizeY, blockSizeX, blockSizeY);
                     break;
-                */
+
+                // "Hindernis fest"                           
+                case "X": 
+                    ctx.drawImage(obstacle, 0, 0, obstacle.width, obstacle.height, x * blockSizeX, y * blockSizeY, blockSizeX, blockSizeY);
+                    break;
+
+                // "Hindernis beweglich" moving between first and second X
+                case "Y": 
+                    
+                    break;
+
+                // First passenger spawning position
+                case "1":
+                    ctx.drawImage(guest, 0, 0, guest.width, guest.height, x * blockSizeX, y * blockSizeY, blockSizeX, blockSizeY);
+                    break;
+
+                // Second passenger spawning position
                 case "2":
                     ctx.drawImage(guest, 0, 0, guest.width, guest.height, x * blockSizeX, y * blockSizeY, blockSizeX, blockSizeY);
                     break;
 
+                // Third passenger spawning position
                 case "3":
                     ctx.drawImage(guest2, Math.floor(frame % 2) * guest2.width / 2, 0, guest2.width / 2, guest2.height,
 			                             x * blockSizeX, y * blockSizeY, blockSizeX, blockSizeY);
                     break;
+
+                // Position for level exit
+                case "E":
+
+                    break;
+
+                // Reserved symbols for our group: G, H, I, J, K, L, M
+
+                // TODO FÜR SPÄTER!!!!!!
+                // Unterscheidung zwischen Groß- und Kleinschreibung
 
             }//switch
         }//for x
