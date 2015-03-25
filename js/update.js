daaf87a62f5faaa3ebc4b1ed3e4b88287ac084a0
matthiasgordon <gordon.matthias@googlemail.com>
2015-03-25 09:56:01 +0100

    Kommentierung bis auf objects.js

--- js/update.js
+++ js/update.js
@@ -52,7 +52,7 @@ function update() {
 
         //Platfrom loop
         for (var i = 0; i < platforms.length; i++) {
-			// Check collision platform, taxi
+			// Check collision platform, taxi / if it collides --> taxi is dead
             if(taxi.currPlatform == 0){
                 if (taxi.collides(platforms[i].xStart, platforms[i].xEnd,
                                    platforms[i].yStart, platforms[i].yEnd)) {
@@ -63,6 +63,7 @@ function update() {
 		
 		//Frame elements loop
 		for(var i = 0; i < frames.length; i++){
+			//If taxi collides with frame --> taxi is dead
 			if(taxi.collides(frames[i].xStart, frames[i].xEnd,
                                    frames[i].yStart, frames[i].yEnd)){
 				taxi.death();
@@ -71,8 +72,10 @@ function update() {
 		
 		//Exit loop
 		for(var i = 0; i < exits.length; i++){
+			//if taxi collides with exit
 			if(taxi.collides(exits[i].xStart, exits[i].xEnd,
                                    exits[i].yStart, exits[i].yEnd)){
+				//and exit is visible --> taxi is dead
 				if(exits[i].state == "visible"){
 					taxi.death();	
 				}
@@ -81,6 +84,7 @@ function update() {
 		
 		//Satellite loop
 		for(var i = 0; i < staticSatellites.length; i++){
+			//If taxi collides with satellite --> taxi dead
 			if(taxi.collides(staticSatellites[i].xStart, staticSatellites[i].xEnd,
                                    staticSatellites[i].yStart, staticSatellites[i].yEnd)){
 				taxi.death();
@@ -89,14 +93,19 @@ function update() {
 		
 		//Powerup loop
 		for(var i = 0; i < powerUps.length; i++){
+			//If taxi collides with powerup
 			if(taxi.collides(powerUps[i].xStart, powerUps[i].xEnd,
                                    powerUps[i].yStart, powerUps[i].yEnd)){
+				//Check if powerup type is Snowden
 				if(powerUps[i].type=="I" && powerUps[i].state == "open"){
+					//Then close the powerup and shut of the transmitter
 					powerUps[i].state = "closed";
 					for(i = 0; i < transmitters.length; i++){
 						transmitters[i].state = "off";
 					}
+				//If the powerup is Lightning
 				}else if(powerUps[i].type=="J" && powerUps[i].state == "open"){
+					//Close the powerup auf set the taxi powerupstate to fast
 					powerUps[i].state = "closed";
 					taxi.powerUpState = "fast";
 				}
@@ -107,24 +116,31 @@ function update() {
 		for (i = 0; i < guests[game.roundNumber-1].length; i++){
 			var currGuest = guests[game.roundNumber-1][i];
 
-			//check if taxi arrived for picking up guest
+			//If the state of the guest is free, the taxi collides with the guest and the taxi does not move in y direction
+			//collect the guest
 			if(currGuest.state == "free"){
 				if (taxi.collides(currGuest.x, currGuest.x + game.blockSize,
 									currGuest.y, currGuest.y + game.blockSize)) {
 					if(taxi.vy == 0){
 						currGuest.state = "onTaxi";
+					//If taxi is faster --> taxi is dead
 					}else{
 						taxi.death();
 					}
 				}
 				
+				//If the current guests platform is equal to the taxis current platform
 				if(currGuest.currPlatform == taxi.currPlatform){
+					//Guest enters the taxi
 					currGuest.enterTaxi();
+				//If they are not on the same platform the guest freezes
 				}else{
 					currGuest.direction = "standing";
 				}	
 			}
-			
+
+			//If the current guest is on the taxi and the taxi lands on the target platform, the guest exits the taxi
+			//and gets the state leaving
 			if(currGuest.state == "onTaxi"){
 				if(game.targetPlatform == taxi.currPlatform && taxi.state == "full"){
 					currGuest.exitTaxi();
@@ -132,10 +148,12 @@ function update() {
 				}
 			}
 			
+			//If the guest is leaving out of the taxi and arrives to his target position on the platform he is "delivered"
 			if(currGuest.state == "leaving"){
 				if (checkCollision(guests[game.roundNumber][0].x, guests[game.roundNumber][0].x + game.blockSize, 
 					guests[game.roundNumber][0].y, guests[game.roundNumber][0].y + game.blockSize, currGuest.x, currGuest.y)){
 						currGuest.state = "delivered";
+				//If he does not arrive he goes to get there
 				}else{
 					currGuest.leaveTaxi();
 				}
@@ -144,6 +162,7 @@ function update() {
 
 		//Drone loop
 		for(i=0; i < drones.length; i++) {
+			//If taxi collides with a drone --> taxi is dead
 	  		if(taxi.collides(drones[i].xStart, drones[i].xEnd, drones[i].yStart, drones[i].yEnd)) {
 	  			taxi.death();
 	  		}
@@ -151,6 +170,7 @@ function update() {
 
 	  	//GoogleCar loop
 	  	for(i=0; i < googleCars.length; i++) {
+	  		//If taxi collides with a googleCar --> taxi is dead
 	  		if(taxi.collides(googleCars[i].xStart, googleCars[i].xEnd, googleCars[i].yStart, googleCars[i].yEnd)) {
 	  			taxi.death();
 	  		}
@@ -158,13 +178,14 @@ function update() {
 
 	  	//Transmitter loop
 	  	for(i=0; i < transmitters.length; i++) {
+	  		//If taxi collides with a transmitter --> taxi is dead
 	  		if(taxi.collides(transmitters[i].xStart, transmitters[i].xEnd, transmitters[i].yStart, transmitters[i].yEnd)) {
 	  			taxi.death();
 	  		}
 	  	}
     }
 
-	//check collision between two objects
+	//check collision between two given objects
     function checkCollision(xStart, xEnd, yStart, yEnd, xObj, yObj) {
         if ((yObj >= yStart && yObj <= yEnd) && (xObj >= xStart && xObj <= xEnd)) {
             return true;
